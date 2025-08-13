a64f21113bac1a2c0a9c47dccdb2eb5c42ef3104
stasi0xx <stanislaw.korycki.w@gmail.com>
2025-08-13 13:26:00 +0200

    assets and metadate

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   console.log("Who am I");
 
   return (
-    <div>
-      <h1 className={"text-3xl text-center font-black"}>Hello World</h1>
+    <div className={"primary-gradient"}>
+      <h1 className={"h1-bold font-inter "}>Hello World</h1>
     </div>
   );
 }
